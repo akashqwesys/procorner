d3e676ac6d1343bc1bc12e8f11d4242127d8665d
vasimlook <vasimlook@gmail.com>
2021-11-26 13:43:23 +0530

    changes
    changes

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,4 +84,6 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-define('API_SECRET', 'WdTTrlSptcivk9XUNGaRpxDBEM1pIjieRZzLtHT3C69TMmk0YBUgiKiIUSXMTyc4');
+// define('API_SECRET', 'WdTTrlSptcivk9XUNGaRpxDBEM1pIjieRZzLtHT3C69TMmk0YBUgiKiIUSXMTyc4');
+define('API_SECRET', 'CGcTyuMrKIaXVqv9S6waKe4bbZNNFONhL1DhxfACJoDVTND1dMuGkla87aN6196c');
+
